55d71efbfdbe72fa0f008ba755d4cfdabaed2e9c
deglebe <tdb@tdbio.me>
2025-06-23 09:25:19 -0400

    neo4j tests

---
+++ neo4j/test.py
@@ -0,0 +1,24 @@
+{
+    'n': {
+        'birthday': neo4j.time.DateTime(1969, 12, 31, 19, 0, 0, 0, tzinfo = pytz.FixedOffset(-300)),
+        'fathersFirstName': 'null',
+        'lastName': 'Roush',
+        'profileFilename': '/users/463c3dc1-6a0f-4457-aa70-3bf37b7faa49/profiles/1737651804810-1d47d5ea-6b62-4288-8f63-22600236165d.jpeg',
+        'emailToken': '15424f005f36e65d7f98c22a14bebb17ec1d3a9b8c0649b833c2bb5f2ee17ecb',
+        'shortId': 'Peggy',
+        'verificationStatus': 'Unverified',
+        'hashedPassword': 'b/GRT76gq1mZiSB91zbdG0GRNCW0Fi',
+        'preferredFullName': 'Peggy Roush',
+        'maidenName': '',
+        'active': True,
+        'title': '',
+        'suffix': '',
+        'userId': '463c3dc1-6a0f-4457-aa70-3bf37b7faa49',
+        'firstName': 'Peggy',
+        'createTime': neo4j.time.DateTime(2025, 1, 23, 17, 0, 54, 870000000, tzinfo = < UTC > ),
+        'permissions': '[{permissionType:USER_PROFILE,permissionLevel:PUBLIC,connectionTypes:[],dof:1,allowedUserIds:[],prohibitedUserIds:[],createdAt:2025-01-23T17:00:54.221Z,updatedAt:2025-01-23T17:00:54.221Z},{permissionType:USER_CONNECTIONS,permissionLevel:PUBLIC,connectionTypes:[],dof:1,allowedUserIds:[],prohibitedUserIds:[],createdAt:2025-01-23T17:00:54.221Z,updatedAt:2025-01-23T17:00:54.221Z},{permissionType:USER_FOLLOWERS,permissionLevel:PUBLIC,connectionTypes:[],dof:1,allowedUserIds:[],prohibitedUserIds:[],createdAt:2025-01-23T17:00:54.221Z,updatedAt:2025-01-23T17:00:54.221Z},{permissionType:USER_STREAM,permissionLevel:FOLLOWERS,connectionTypes:[],dof:1,allowedUserIds:[],prohibitedUserIds:[],createdAt:2025-01-23T17:00:54.221Z,updatedAt:2025-01-23T17:00:54.221Z}]',
+        'middleName': '',
+        'emailValidated': False,
+        'email': 'peggy@email.com'
+    }
+}
